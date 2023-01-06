8f7c879c9d40eae3bc1424eedb82f3262e14678e
LeeJunSang <dev.enjoy.art@gmail.com>
2023-01-06 18:03:26 +0000

    230106 day07

---
+++ workspace/job/src/job/Test.java
@@ -0,0 +1,17 @@
+package job;
+
+import java.util.Random;
+
+public class Test {
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10));
+		
+	}
+}
