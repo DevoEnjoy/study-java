92faa579a1f506cea9829e770e26ee12f3cc1984
LeeJunSang <dev.enjoy.art@gmail.com>
2023-01-17 14:53:10 +0900

    auto push 20230117 1453

--- workspace/file/src/fileTest/FileTest.java
+++ workspace/file/src/fileTest/FileTest.java
@@ -1,8 +1,7 @@
 package fileTest;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileTest {
@@ -10,8 +9,8 @@ public class FileTest {
 
 //		절대 경로: 어디에서 작성해도 찾아갈 수 있는 경로, C:/a/b
 //		상대 경로: 현재 위치에 따라 변경되는 경로, ../a/b
-		BufferedWriter bufferedWriter = null;
-		bufferedWriter = new BufferedWriter(new FileWriter("src/test.txt"));
+//		BufferedWriter bufferedWriter = null;
+//		bufferedWriter = new BufferedWriter(new FileWriter("src/test.txt"));
 
 //		if (bufferedWriter != null) {
 //		bufferedWriter.close();
@@ -20,9 +19,16 @@ public class FileTest {
 //		System.runFinalization();
 //		}
 		
-		File file = new File("src/test.txt");
-		if (file.exists()) {
-			file.delete();
+//		File file = new File("src/test.txt");
+//		if (file.exists()) {
+//			file.delete();
+//		}
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("src/test.txt"));
+		String line = null;
+		while((line = bufferedReader.readLine()) != null) {
+			System.out.println(line);
 		}
+		
+		
 	}
 }
