8a71727c05619db8ddc863a3c17609f258633111
LeeJunSang <dev.enjoy.art@gmail.com>
2023-02-07 17:28:45 +0900

    auto push 20230207 1728

--- workspace/myLambda/src/lambdaTest/PrintNameTest.java
+++ workspace/myLambda/src/lambdaTest/PrintNameTest.java
@@ -1,13 +1,20 @@
 package lambdaTest;
 
+import java.util.stream.IntStream;
+
 public class PrintNameTest {
-	public void printFullName(PrintName printName) {
-		System.out.println(printName.getFullName("이", "순신"));
+	public void printFullName(PrintName printName, String firstName, String lastName) {
+		System.out.println(printName.getFullName(firstName, lastName));
 	}
-	
+
 	public static void main(String[] args) {
 		PrintNameTest printNameTest = new PrintNameTest();
-		printNameTest.printFullName((f, l) -> l + f);
+//		printNameTest.printFullName((f, lastName) -> lastName + f);
+//		printNameTest.printFullName((f, l) -> f + l + "님 어서오세요", "이", "순신");
+//		printNameTest.printFullName(((f, l) -> f + l + "님 어서오세요"), "이", "순신");
+
+		IntStream.range(0, 10).map(v -> (char) (v + 'a')).forEach(v -> System.out.println((char) v));
+
 	}
 
 }
